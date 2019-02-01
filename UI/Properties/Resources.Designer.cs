525c23ece9d6518653d1d934ced1532f61ac118b
Juan <Juan@JUAN-PC>
2019-01-31 23:17:52 -0600

    .

--- UI/Properties/Resources.Designer.cs
+++ UI/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace UI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Add_icon {
-            get {
-                object obj = ResourceManager.GetObject("Add-icon", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap asoc {
             get {
                 object obj = ResourceManager.GetObject("asoc", resourceCulture);
